dc39aae2eb914ba63cd6657d211ec691660611cf
Leon Arantes <64018468+LeonArantes@users.noreply.github.com>
2021-06-15 22:49:41 -0300

    chore: install typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+        <h1>Config Typescript</h1>
+      </main>
+    </div>
+  )
+}
